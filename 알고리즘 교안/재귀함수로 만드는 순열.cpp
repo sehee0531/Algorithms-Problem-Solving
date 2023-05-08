c6df1088894a04096a5d675bd445efe4ca1e8d41
wjdtpgml <wjdtpgml0531@gmail.com>
2023-05-08 22:42:04 +0900

    조합/재귀함수

--- 알고리즘 교안/재귀함수로 만드는 순열.cpp
+++ 알고리즘 교안/재귀함수로 만드는 순열.cpp
@@ -1,35 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;    
-int a[3] = {1, 2,3};
-vector<int> v; 
-void printV(vector<int> &v){
-    for(int i = 0; i < v.size(); i++){
-        cout << v[i] << "";
-    }
-    cout << "\n";
+/*#include<bits/stdc++.h>
+using namespace std;
+vector<int> v={1,2,3};
+int main(){
+	do{
+		for(int i:v){
+			cout<<i;
+		}
+		cout<<"\n";
+	}while(next_permutation(v.begin(),v.end()));
+	
+}*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[3]={1,2,3};
+vector<int> b;
+
+void printV(vector<int> &b){
+	for(int i=0;i<b.size();i++){
+		cout<<b[i]<"\n";
+	}
+	cout<<"\n";
 }
-void makePermutation(int n, int r, int depth){
-    if(r == depth){
-        printV(v);
-        return;
-    }
-    for(int i = depth; i < n; i++){ 
-        swap(v[i], v[depth]);
-        
-        cout << i << " : " << depth << " ¹Ù²Û´Ù!!\n";
-        printV(v);
-        
-        makePermutation(n, r, depth + 1);
-        swap(v[i], v[depth]);
-        
-        cout << i << " : " << depth << " ´Ù½Ã ¹Ù²Û´Ù!!\n"; 
-        printV(v);
-        printV(v);
-    }
-    return; 
+void makePermutation(int n,int r, int depth){
+	if(r==depth){
+		printV(b);
+		return;
+	}
+	
+	for(int i=depth;i<n;i++){
+		swap(b[i],b[depth]);
+		makePermutation(n,r,depth+1);
+		swap(b[i],b[depth]);
+	}
 }
-int main(){ 
-    for(int i = 0; i < 3; i++)v.push_back(a[i]); 
-    makePermutation(3, 3, 0); 
-    return 0; 
-} 
+
+int main(){
+	for(int i=0;i<3;i++)b.push_back(a[i]);
+	makePermutation(3,3,0);
+	return 0;
+}
+
+
---
+++ 알고리즘 교안/재귀함수로 만드는 조합.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n=5; int k=3;
+
+void printV(vector<int> v){
+	for(int i : v) cout<<i<<" ";
+	cout<<"\n";
+}
+
+void combi(int start, vector<int> v){
+	if(v.size()==k)
+	{
+		printV(v);
+		return;
+	}
+	
+	for(int i=start+1;i<n;i++){
+		v.push_back(i);
+		combi(i,v);
+		v.pop_back();
+	}
+	
+	
+}
+int main(){
+	vector<int> v;
+	combi(-1,v);
+	return 0;
+}
